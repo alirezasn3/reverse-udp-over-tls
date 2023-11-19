4efa2f7ea49891253c6ea5624560e06b1f96268f
Alireza Saadatnia <alirezasn3@gmail.com>
2023-11-20 00:29:04 +0330

    added admin dashboard

--- main.go
+++ main.go
@@ -3,11 +3,22 @@ package main
 import (
 	"crypto/tls"
 	"encoding/json"
+	"fmt"
+	"net/http"
 	"os"
 	"sync"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
 )
 
 var GlobalConfig Config
+var servers []*Server
+var totalDwonload uint64 = 0
+var totalUpload uint64 = 0
+var CurrentDownload uint64 = 0
+var CurrentUpload uint64 = 0
 
 type Config struct {
 	Role                string   `json:"role"`
@@ -15,6 +26,7 @@ type Config struct {
 	UDPConnect          string   `json:"udpConnect"`
 	TCPListen           string   `json:"tcpListen"`
 	UDPListen           string   `json:"udpListen"`
+	MonitorAddress      string   `json:"monitorAddress"`
 	CertificateLocation string   `json:"certificateLocation"`
 	KeyLocation         string   `json:"keyLocation"`
 	TLSConfig           tls.Config
@@ -61,9 +73,66 @@ func main() {
 			wg.Add(1)
 			go func(a string) {
 				s := Server{ClientAddress: a}
+				servers = append(servers, &s)
 				s.Run()
 			}(clientAddress)
 		}
+		wg.Add(1)
+		go func() {
+			for range time.NewTicker(time.Second).C {
+				totalDwonload = 0
+				totalUpload = 0
+				CurrentDownload = 0
+				CurrentUpload = 0
+				for _, s := range servers {
+					s.CurrentDownload = s.TotalDownload - s.D
+					s.CurrentUpload = s.TotalUpload - s.U
+					s.D = s.TotalDownload
+					s.U = s.TotalUpload
+					totalDwonload += s.TotalDownload
+					totalUpload += s.TotalUpload
+					CurrentDownload += s.CurrentDownload
+					s.CurrentUpload += s.CurrentDownload
+				}
+			}
+		}()
+		wg.Add(1)
+		go func() {
+			router := gin.Default()
+			router.LoadHTMLGlob("templates/*")
+			router.GET("/", func(c *gin.Context) {
+				c.HTML(http.StatusOK, "index.html", gin.H{
+					"servers":             servers,
+					"tcpConnect":          GlobalConfig.TCPConnect,
+					"udpConnect":          GlobalConfig.UDPConnect,
+					"certificateLocation": GlobalConfig.CertificateLocation,
+					"keyLocation":         GlobalConfig.KeyLocation,
+					"currentDownload":     CurrentDownload,
+					"currentUpload":       CurrentUpload,
+					"totalDownload":       totalDwonload,
+					"totalUpload":         totalUpload,
+				})
+			})
+			router.GET("/ws", func(ctx *gin.Context) {
+				upgrader := websocket.Upgrader{CheckOrigin: func(r *http.Request) bool { return true }}
+				conn, err := upgrader.Upgrade(ctx.Writer, ctx.Request, nil)
+				if err != nil {
+					http.Error(ctx.Writer, "Could not open websocket connection", http.StatusBadRequest)
+					fmt.Println(err)
+				}
+				for {
+					time.Sleep(time.Second)
+					conn.WriteJSON(map[string]interface{}{
+						"servers":         servers,
+						"currentDownload": CurrentDownload,
+						"currentUpload":   CurrentUpload,
+						"totalDownload":   totalDwonload,
+						"totalUpload":     totalUpload,
+					})
+				}
+			})
+			router.Run(GlobalConfig.MonitorAddress)
+		}()
 		wg.Wait()
 	} else if GlobalConfig.Role == "client" {
 		c := Client{}
--- server.go
+++ server.go
@@ -11,40 +11,70 @@ import (
 )
 
 type Server struct {
-	ClientAddress string
+	ClientAddress        string
+	TotalDownload        uint64
+	TotalUpload          uint64
+	AttemptedConnections uint64
+	AcceptedConnections  uint64
+	ActiveConnections    uint64
+	CurrentDownload      uint64
+	CurrentUpload        uint64
+	LatestConnection     int64
+	Status               string
+	D                    uint64 // to help calculate current donwload
+	U                    uint64 // to help calculate current upload
 }
 
 func (s *Server) Run() {
+	s.Status = "down"
 	lastLog := time.Now()
 	for {
 		connectionToClient, e := s.CreateConnection()
 		if e != nil {
 			if e != io.EOF && !errors.Is(e, syscall.ECONNRESET) && time.Since(lastLog).Milliseconds() > 1000 {
 				fmt.Printf("[%s] failed to create new connection to %s\n", e.Error(), s.ClientAddress)
+				if s.Status == "up" {
+					s.Status = "down"
+				}
+			} else {
+				if s.Status == "down" {
+					s.Status = "up"
+				}
 			}
 		} else {
 			fmt.Printf("created new connection to client to %s\n", s.ClientAddress)
 
+			s.LatestConnection = time.Now().Unix()
+
 			// handle connection to client on new go routine
 			go s.HandleConnection(connectionToClient)
+
+			if s.Status == "down" {
+				s.Status = "up"
+			}
 		}
 		time.Sleep(time.Millisecond * 200)
 	}
 }
 
 func (s *Server) CreateConnection() (*tls.Conn, error) {
+	s.AttemptedConnections++
 	// connect to client
 	c, e := tls.DialWithDialer(&net.Dialer{Timeout: time.Second * 1}, "tcp", s.ClientAddress, &GlobalConfig.TLSConfig)
 	if e != nil {
 		return nil, e
 	}
-
+	s.AcceptedConnections++
+	s.ActiveConnections++
 	return c, nil
 }
 
 func (s *Server) HandleConnection(connectionToClient *tls.Conn) {
 	// close connection to client when done
-	defer connectionToClient.Close()
+	defer func() {
+		s.ActiveConnections--
+		connectionToClient.Close()
+	}()
 
 	// parse local service address
 	localServiceAddress, err := net.ResolveUDPAddr("udp4", GlobalConfig.UDPConnect)
@@ -81,6 +111,7 @@ func (s *Server) HandleConnection(connectionToClient *tls.Conn) {
 			if e != nil {
 				return
 			}
+			s.TotalUpload += uint64(n)
 
 			// write packet to local service
 			_, e = connectionToLocalService.Write(b[:n])
@@ -106,6 +137,7 @@ func (s *Server) HandleConnection(connectionToClient *tls.Conn) {
 		if e != nil {
 			return
 		}
+		s.TotalDownload += uint64(n)
 
 		// write packet to client
 		_, e = connectionToClient.Write(b[:n])
