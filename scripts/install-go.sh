2ee30b6046c396c573d46cb0302ae55327778336
Alireza Saadatnia <alirezasn3@gmail.com>
2024-01-10 10:12:56 +0330

    added instruction to use source

--- scripts/install-go.sh
+++ scripts/install-go.sh
@@ -5,5 +5,5 @@ curl -OL https://golang.org/dl/go1.20.3.linux-amd64.tar.gz
 sudo tar -C /usr/local -xvf go1.20.3.linux-amd64.tar.gz
 rm go1.20.3.linux-amd64.tar.gz
 echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.profile
-source ~/.profile
+echo "Run this command to complete installation: source ~/.profile"
 go version
