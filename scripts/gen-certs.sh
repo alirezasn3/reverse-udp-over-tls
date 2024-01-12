3d77f8a28fe555289da5a56e10b25680c68e0286
Alireza Saadatnia <alirezasn3@gmail.com>
2024-01-12 22:41:44 +0330

    fixed ws endpoint

--- scripts/gen-certs.sh
+++ scripts/gen-certs.sh
@@ -6,3 +6,4 @@ echo "make server cert"
 openssl req -new -nodes -x509 -out certs/server.pem -keyout certs/server.key -days 3650 -subj "/C=DE/ST=NRW/L=Earth/O=Example Company/OU=IT/CN=www.example.com/emailAddress=john@example.com"
 echo "make client cert"
 openssl req -new -nodes -x509 -out certs/client.pem -keyout certs/client.key -days 3650 -subj "/C=DE/ST=NRW/L=Earth/O=Example Company/OU=IT/CN=www.example.com/emailAddress=john@example.com"
+mv certs/* ../certs/
